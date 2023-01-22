4cf88ad60e67b4aba2dbf5b420d8defbe57e5f88
sami <admin@seattledevicerepair.com>
2023-01-21 22:19:01 -0800

    try fix

--- social_site/settings.py
+++ social_site/settings.py
@@ -105,13 +105,23 @@ CHANNEL_LAYERS = {
 }
 
 
-# Database
+# Databased
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'XcEPiMfOcaqVOeZcZpTd',
+        'HOST': 'containers-us-west-114.railway.app',
+        'PORT': '5529',
     }
 }
 
