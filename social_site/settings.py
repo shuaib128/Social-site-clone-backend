b9d53969195c433287aa45fd908d7193d61be3a2
sami <admin@seattledevicerepair.com>
2023-01-31 14:52:25 -0800

    added static files

--- social_site/settings.py
+++ social_site/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-u2kdunhwe6^-)_dl9c02gn@a#l2$!^y3=d8+anm#y6*#!1*z=k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
