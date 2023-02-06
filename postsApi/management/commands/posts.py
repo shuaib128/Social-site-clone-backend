5926172971ec0485902fa619cb51a1c24d8d898b
sami <admin@seattledevicerepair.com>
2023-02-05 16:33:04 -0800

    static

--- postsApi/management/commands/posts.py
+++ postsApi/management/commands/posts.py
@@ -8,14 +8,14 @@ from users.models import Profile
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        title_path = r"E:\Django Projects\3. socialsite_with_api\site\postsApi\management\contents\titles.txt"
+        title_path = r"D:\Django Projects\3. socialsite_with_api\site\postsApi\management\contents\titles.txt"
 
         with open(title_path) as title:
             lines = title.readlines()
             count = 0
 
             for index, line in enumerate(lines):
-                id_ = id=random.randint(2, 4)
+                id_ = id=random.randint(5, 6)
                 Post.objects.create(
                     title = line,
                     description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
