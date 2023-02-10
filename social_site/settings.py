42343138b70bbb900987839703be89833abddf54
sami <admin@seattledevicerepair.com>
2023-02-09 23:34:28 -0800

    removes int vars

--- social_site/settings.py
+++ social_site/settings.py
@@ -16,10 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-u2kdunhwe6^-)_dl9c02gn@a#l2$!^y3=d8+anm#y6*#!1*z=k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -116,9 +116,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'PASSWORD': "06MbDr4ocBtEZS8IFEGY",
+        'HOST': "containers-us-west-114.railway.app",
+        'PORT': "5529",
     }
 }
 
